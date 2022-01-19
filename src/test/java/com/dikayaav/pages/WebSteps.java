bfc896edf361d9caf976a37426c8b687916e4e76
SashkaDikaya <dikayaav@gmail.com>
2022-01-19 23:38:42 +0300

    add selenide test, lambda & web. final

--- src/test/java/com/dikayaav/pages/WebSteps.java
+++ src/test/java/com/dikayaav/pages/WebSteps.java
@@ -1,11 +1,18 @@
-package com.dikayaav.tests;
+package com.dikayaav.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 
+import java.nio.charset.StandardCharsets;
+
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
 
 public class WebSteps {
 
@@ -26,14 +33,26 @@ public class WebSteps {
         $(linkText(repository)).click();
     }
 
-    @Step("Проверяем что открылся репозиторий {repository}")
-    public void checkRepositoryName (String repository){
-        $("#repository-container-header").shouldHave(text(repository));
-    }
-
     @Step("Проверяем что на странице есть таб  {issues}")
     public void checkIssuesTab (String issues){
         $("#repository-container-header").shouldHave(text(issues));
     }
+
+    @Step("Нажимаем на таб  {issues}")
+    public void pressIssuesTab (String issues){
+        $(partialLinkText(issues)).click();
+    }
+
+    @Step("Проверяем наличие Issue {num}")
+    public void checkIssue (String num){
+        $(withText(num)).should(Condition.visible);
+    }
+
+    @Attachment(value = "Screenshot", type = "text/html", fileExtension = "html")
+    public byte[] attachPageSource() {
+        return WebDriverRunner.source().getBytes(StandardCharsets.UTF_8);
+    }
+
+
 
 }
--- src/test/java/com/dikayaav/tests/SelenideTest.java
+++ src/test/java/com/dikayaav/tests/SelenideTest.java
@@ -1,25 +1,38 @@
 package com.dikayaav.tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
 
 public class SelenideTest {
 
     @Test
+    @Owner("dikayaav")
+    @Feature("Issue")
+    @Story("Проверка Issue в репозитории eroshenkoam/allure-example")
+    @DisplayName("Check issue in repository eroshenkoam/allure-example")
+    @Severity(SeverityLevel.BLOCKER)
+
     void WebTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Selenide.open("https://github.com");
         $(".header-search-input").click();
-        $(".header-search-input").sendKeys("SashkaDikaya/QAGuruTask2");
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
         $(".header-search-input").submit();
-        $(linkText("SashkaDikaya/QAGuruTask2")).click();
-        $("#repository-container-header").shouldHave(text("SashkaDikaya/QAGuruTask2"));
-        $("#issues-tab").shouldHave(text("Issues"));
-        $("#issues-tab").click();
-
+        $(linkText("eroshenkoam/allure-example")).click();
+        Allure.addAttachment("Page Source", "text/html", WebDriverRunner.source(), "html");
+        $(partialLinkText("Issues")).click();
+        $(withText("#53")).should(Condition.visible);
 
     }
 }
