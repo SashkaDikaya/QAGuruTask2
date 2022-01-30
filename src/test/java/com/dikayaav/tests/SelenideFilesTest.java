0d1364cc7b1ea4923dbda10961cce388dc34d0d3
SashkaDikaya <dikayaav@gmail.com>
2022-01-30 12:10:24 +0300

    some changes

--- src/test/java/com/dikayaav/tests/SelenideFilesTest.java
+++ src/test/java/com/dikayaav/tests/SelenideFilesTest.java
@@ -4,12 +4,15 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class SelenideFilesTest {
 
     @Test
-    void downloadTest(){
+    void downloadTest() throws FileNotFoundException {
+        Selenide.open("https://github.com/junit-team/junit5/blob/main/LICENSE.md");
         File downloadFile = Selenide.$("#raw-url").download();
+        System.out.println();
     }
 
 }
