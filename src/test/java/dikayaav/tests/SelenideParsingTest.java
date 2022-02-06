78d1c73e5abdf7589276bdc5b73cb9629620447f
SashkaDikaya <dikayaav@gmail.com>
2022-02-06 16:35:17 +0300

    some changes

--- src/test/java/dikayaav/tests/SelenideParsingTest.java
+++ src/test/java/dikayaav/tests/SelenideParsingTest.java
@@ -21,7 +21,7 @@ public class SelenideParsingTest {
     //private ClassLoader cl = getClass().getClassLoader();
 
     @Test
-    void parseCsvTest() throws Exception {
+    void parseTests() throws Exception {
 
         ZipFile zipFile = new ZipFile("src/test/resources/Downloads.zip");
 
@@ -32,52 +32,28 @@ public class SelenideParsingTest {
             assertThat(parsed.numberOfPages).isEqualTo(231);
             assertThat(parsed.text).contains("BoxLayout - расположение по горизонтали или вертикали");
         }
-    }
 
-   /* @Test
-    void parsePdfTest() throws Exception {
-        Selenide.open("https://junit.org/junit5/docs/current/user-guide/");
-        File pdfDownload = $(byText("PDF download")).download();
-        PDF parsed = new PDF(pdfDownload); //еще создаем переменную для распарсенного файла
-        assertThat(parsed.author).contains("Marc Philip");
-    }
-
-    @Test
-    void parseXlsTest() throws Exception {
-        try (InputStream stream = cl.getResourceAsStream("sample-xlsx-file.xlsx")) {
-            XLS parsed = new XLS(stream);
-            assertThat(parsed.excel.getSheetAt(0).getRow(1).getCell(2).getStringCellValue())
-                    .isEqualTo("Abril");
+        ZipEntry zipXlsEntry = zipFile.getEntry("sample-xlsx-file.xlsx");
+        try (InputStream inputXlsStream = zipFile.getInputStream(zipXlsEntry)) {
+            XLS parsed = new XLS(inputXlsStream);
+            assertThat(parsed.excel.getSheetAt(0).getRow(3).getCell(4).getStringCellValue())
+                    .isEqualTo("France");
+            assertThat(parsed.excel.getSheetAt(0).getRow(22).getCell(1).getStringCellValue())
+                    .isEqualTo("Holly");
         }
-    }
 
-    @Test
-    void parseCsvTest() throws Exception {
-        try (InputStream stream = cl.getResourceAsStream("example.csv")) {
-            CSVReader reader = new CSVReader(new InputStreamReader(stream));
+        ZipEntry zipCsvEntry = zipFile.getEntry("sample.csv");
+        try (InputStream inputCsvStream = zipFile.getInputStream(zipCsvEntry)) {
+            CSVReader reader = new CSVReader(new InputStreamReader(inputCsvStream));
             List<String[]> list = reader.readAll();
             assertThat(list)
-                    .hasSize(3)
+                    .hasSize(51)
                     .contains(
-                            new String[]{"Author", "Book"},
-                            new String[]{"Block", "Apteka"},
-                            new String[]{"Esenin", "Cherniy Chelovek"}
-                    );
+                            new String[]{"0;First Name;Last Name;Gender;Country;Age;Date;Id"},
+                            new String[]{"33;Jona;Grindle;Female;Great Britain;26;21/05/2015;6458"},
+                            new String[]{"50;Rasheeda;Alkire;Female;United States;29;16/08/2016;6125"});
         }
     }
-
-    @Test
-    void parsedZipTest() throws Exception {
-        try (InputStream stream = cl.getResourceAsStream("sample-zip-file.zip");
-             ZipInputStream zis = new ZipInputStream(stream)) {
-            ZipEntry zipEntry;
-            while ((zipEntry = zis.getNextEntry()) != null) {
-                assertThat(zipEntry.getName()).isEqualTo("sample.txt");
-
-
-            }
-        }*/
-
 }
 
 
