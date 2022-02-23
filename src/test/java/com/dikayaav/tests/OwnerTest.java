f680238d6f64556017c835d437690e13de7b87cf
SashkaDikaya <dikayaav@gmail.com>
2022-02-23 14:17:44 +0300

    some changes

---
+++ src/test/java/com/dikayaav/tests/OwnerTest.java
@@ -0,0 +1,25 @@
+package com.dikayaav.tests;
+
+import com.dikayaav.config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+
+public class OwnerTest {
+
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+    @Test
+    void loginTest() {
+        String login = config.login();
+        String password = config.password();
+
+        System.out.println("Login: " + login);
+        System.out.println("Password: " + password);
+
+//        String message = "I logged in as " + login + " with password " + password;
+        String message = format("I logged in as %s with password %s", login, password);
+        System.out.println(message);
+    }
+}
