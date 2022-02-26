953b1615eda82fb8628b6ae0bb23b7d07e00b21d
SashkaDikaya <dikayaav@gmail.com>
2022-02-26 20:12:21 +0300

    add selenoid tests

---
+++ src/test/java/tests/HomeWorkTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeWorkTests {
+
+        /*
+        Request url:
+        https://reqres.in/api/users?page=2
+
+         */
+
+    @Test
+    void listUsersTest () {
+
+
+    }
+
+}
